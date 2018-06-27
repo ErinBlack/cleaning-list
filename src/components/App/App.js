633fe9b057d8db2a5166017cb697e038f420137d
Erin Black <erin.black@fieldnation.com>
2018-06-26 20:31:15 -0500

    got all checkboxes on their own line

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -13,11 +13,15 @@ class App extends Component {
   }
   choreListItemArray
   render() {
-    let choreListItemArray = this.state.choreList.map(chore => <label><input type="checkbox" value={chore}/> {chore}</label>);
+    let choreListItemArray = this.state.choreList.map(chore => {
+      return (<label className="c-checklist__item">
+                <input type="checkbox" value={chore}/> {chore} <br />
+              </label>
+       )});
     console.log('choreListItemArray', choreListItemArray)
         return (
       <div className="App">
-        <form>
+        <form >
            {choreListItemArray}
        </form>
       </div>
