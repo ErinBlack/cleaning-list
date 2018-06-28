49c1093e7c5c38009bca718b5b126e64af604bde
Erin Black <erin.black@fieldnation.com>
2018-06-28 12:14:35 -0500

    transfered chores into objects

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,16 +6,25 @@ class App extends Component {
     super(props);
     this.state = {
       choreList: [
-      'Clean Kitchen Surfaces', 
-      'Vaccuum Dining Room Rug', 
-      'Clean Thorin\'s Cage'
+        {
+          name: 'Clean Kitchen Surfaces',
+          lastCompleted: '05/29/2018 22:50:4'
+        },
+        {
+          name: 'Vaccuum Dining Room Rug',
+          lastCompleted: '05/27/2018 17:50:4'
+        },
+        {
+          name: 'Clean Thorin\'s Cage',
+          lastCompleted: '05/24/2018 17:50:4'
+        } 
       ],
     };
   }
   choreListItemArray
   render() {
     let choreListItemArray = this.state.choreList.map((chore, i) => {
-      return (<ChoreItem key={i} chore={chore} />)
+      return (<ChoreItem key={i} chore={chore.name} />)
     });
     console.log('choreListItemArray', choreListItemArray)
         return (
